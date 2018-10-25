038c0ada4067038532b9988b7e09108fd3658850
andres garcia <you@example.com>
2018-10-25 14:25:33 -0400

    Finish adding images, and re doing UI.Also made it responsive

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# Movie-Info
-This is a movie listing application.
+# SEARCHFLiX
+This responsive movie listing application that utilises TMDB API to search and find movies.
+To create this app I used HTML5, CSS3,  customized version of bootstrap (Bootswatch), jQuery with Javascript ES6, along with Axios for making HTTP request to the TMDB API.
--- js/main.js
+++ js/main.js
@@ -3,69 +3,70 @@ $(document).ready(() => {
         let searchText = $("#searchText").val();
         getMovies(searchText);
         e.preventDefault();
+
     });
 });
-
 function getMovies(searchText) {
     axios.get("http://api.themoviedb.org/3/search/movie?api_key=98325a9d3ed3ec225e41ccc4d360c817&language=en-US&query=" + searchText)
-    .then((response) => {
-        let movies = response.data.results;
-        let output = "";
-        $.each(movies, (index, movie) => {
-            output += `
+        .then((response) => {
+            console.log(response)
+            let movies = response.data.results;
+            let output = "";
+            $.each(movies, (index, movie) => {
+                output += `
             <div class="col-md-3">
                 <div class="well text-center">
                     <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}">
                     <h5>${movie.title}</h5>
-                    <a onclick="movieSelected('${movies.id}')" class="btn btn-primary" href="#">Movie Details</a>
+                    <a onclick="movieSelected('${movie.id}')" class="btn btn-danger" href="#">Movie Details</a>
                 </div>
             </div>
             `;
-        });
+            });
 
-        $( '#movies').html(output);
-    })
-    .catch((err) => {
-        console.log(err);
-    });
+            $('#movies').html(output);
+        })
+        .catch((err) => {
+            console.log(err);
+        });
 
 }
 
 function movieSelected(id) {
-    sessionStorage.setItem("movieId" , id);
+    sessionStorage.setItem("movieId", id);
     window.location = "movie.html";
     return false;
 }
 
 function getMovie() {
     let movieId = sessionStorage.getItem("movieId");
-    // Make a request for a user with a given ID
+
+    // Make a request for a movie with a given ID
     axios.get("https://api.themoviedb.org/3/movie/" + movieId + "?api_key=98325a9d3ed3ec225e41ccc4d360c817")
         .then((response) => {
-            console.log(movie);
+            console.log(response.data);
             let movie = response.data;
 
-            let output =`
-                <div class "row">
-                     <div class="col-md-4">
-                        <img src=""https://image.tmdb.org/t/p/w500${movie.poster_path}" class="thumbnail">
+            let output = `
+                <div class ="row">
+                    <div class="col-md-4">
+                        <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}" class="thumbnail">
                     </div>
                     <div class="col-md-8">
                         <h2>${movie.title}</h2>
                         <ul class="list-group">
+                        <li class="list-group-item"><strong>Genre:</strong> ${movie.genres[0].name}</li>
                             <li class="list-group-item"><strong>Released:</strong> ${movie.release_date}</li>
                             <li class="list-group-item"><strong>Rated:</strong> ${movie.vote_average}</li>
-                            <li class="list-group-item"><strong>Runtime:</strong> ${movie.runtime} min.</li>
-                            <li class="list-group-item"><strong>Production Companies:</strong> ${movie.production_companies[0].name} min.</li>
+                            <li class="list-group-item"><strong>Runtime:</strong> ${movie.runtime}min</li>
+                            <li class="list-group-item"><strong>Production Companies:</strong> ${movie.production_companies[0].name}</li>
                         </ul>
                     </div>
-                </div>
-                <div class="row">
-                    <div class="well">
+                    <div class="col-md-6">
                         <h3>Plot</h3>
                         ${movie.overview}
                         <hr>
-                        <a href="http://imdb.com/title/${movie.imdb_id}" target="_blank" class="btn btn-primary">View In IMDB</a>
+                        <a href="http://imdb.com/title/${movie.imdb_id}" target="_blank" class="btn btn-danger">View In IMDB</a>
                         <a href="index.html" class="btn btn-default">Go Back To Search</a>
                     </div>
                 </div>
